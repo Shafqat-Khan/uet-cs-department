05426121b34a9dd4cefad6a216094e9b7a303000
Shafqat Ullah <shafqat333khan@gmail.com>
2023-09-23 17:25:47 +0500

    update

--- src/app/components/articles/display-article/display-article.component.ts
+++ src/app/components/articles/display-article/display-article.component.ts
@@ -10,46 +10,58 @@ import { Article } from '../article.model';
   templateUrl: './display-article.component.html',
   styleUrls: ['./display-article.component.css'],
 })
-export class DisplayArticleComponent implements OnInit{
-private mode: string = 'create';
-private articleId!: string;
-article: any;
-isLoading = false;
+export class DisplayArticleComponent implements OnInit {
+  private mode: string = 'create';
+  private articleId!: string;
+  article: any;
+  isLoading = false;
 
-constructor(public articlesService: ArticlesService, public route: ActivatedRoute) {}
+  constructor(
+    public articlesService: ArticlesService,
+    public route: ActivatedRoute
+  ) {}
 
-ngOnInit() {
-  this.route.paramMap.subscribe((paramMap: ParamMap) => {
-    if (paramMap.has('articleId')) {
-      this.mode = 'edit';
-      this.articleId = paramMap.get('articleId')!;
-      this.isLoading = true;
-      this.articlesService.getOneArticle(this.articleId!).subscribe(articleData => {
-        this.isLoading = false;
-        this.article = {id: articleData._id, link: articleData.link, heading: articleData.heading};
-      });
-    } else {
-      this.mode = 'create';
-      this.articleId = "";
-    }
-  });
-}
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('articleId')) {
+        this.mode = 'edit';
+        this.articleId = paramMap.get('articleId')!;
+        this.isLoading = true;
+        this.articlesService
+          .getOneArticle(this.articleId!)
+          .subscribe((articleData) => {
+            this.isLoading = false;
+            this.article = {
+              id: articleData._id,
+              link: articleData.link,
+              heading: articleData.heading,
+            };
+          });
+      } else {
+        this.mode = 'create';
+        this.articleId = '';
+      }
+    });
+  }
 
   onAddText(form: NgForm) {
     if (form.invalid) {
-      console.log("incva");
+      console.log('incva');
       return;
     }
-    if(this.mode === "create"){
+    if (this.mode === 'create') {
       this.isLoading = false;
 
       this.articlesService.addArticle(form.value.link, form.value.heading);
     } else {
-      console.log("22222");
-      this.articlesService.updateArticle(this.articleId,form.value.link, form.value.heading);
+      console.log('22222');
+      this.articlesService.updateArticle(
+        this.articleId,
+        form.value.link,
+        form.value.heading
+      );
     }
 
     form.resetForm();
   }
 }
- 
